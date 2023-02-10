137f3cea016e813778ab1b9855e8c0f5b368fa25
malik672 <aremumalik01@gmail.com>
2023-02-10 23:43:07 +0100

    filter

--- src/arrayMethod.sol
+++ src/arrayMethod.sol
@@ -142,4 +142,23 @@ library Methods {
             }
         }
     }
+
+    ///@dev takes in array an array and a function and return a the filtered item in a new array
+    ///@param arr takes in array of unsigned Integers
+    ///@param func takes in a func as a parameter
+    function filter(uint256[] memory arr, function(uint256[] memory) external pure func)
+        external
+        pure
+        returns (uint256 z)
+    {
+        assembly{
+            //get where array is stored in array
+            let location := arr
+
+            //get length of array
+            let length := mload(arr)
+
+            //
+        }
+    }
 }
